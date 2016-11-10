33e3f52669d1b76eefeb135f18de5145db09766e
qiaodaniMac <qiaodandan@QiaodabdandeiMac.local>
2016-11-10 14:36:41 +0800

    颜色小游戏Demo的随机算法优化，但是还不是符合实际情况的算法，还需要设计颜色逐渐接近的算法

--- MyCatPro/Colors/colorApp.js
+++ MyCatPro/Colors/colorApp.js
@@ -12,10 +12,14 @@ var n = 2;
 function addRects(){
 
   //取两个颜色的算法,还需要优化-----难度算法的重写----
-  var cl = parseInt(Math.random()*1000000)
+  //随机的没有问题,但是并没有随着难度的增加,颜色慢慢相近的效果
+  var cl = getRandomColorNO();
   var color = "#"+cl;
-  var cl2 = parseInt(Math.random()*1000000)
+
+  var cl2 = getOnlyColor(n,cl);
   var onlyColor = "#"+cl2;
+
+
   var x = parseInt(Math.random()*n);
   var y = parseInt(Math.random()*n);
 //取两个颜色的算法,还需要优化---------
@@ -41,6 +45,66 @@ function addRects(){
         })
       }
     }
+  }
+  //根绝难度等级返回 the only color
+  function getOnlyColor(n,color){
+   var onlyColorNo;
+    switch (n){
+      case 2:
+        onlyColorNo = getRandomColorNO();
+        break;
+      case 3:
+        onlyColorNo = getAfterChangeColor(color,5);
+        break;
+      case 4:
+        onlyColorNo = getAfterChangeColor(color,4);
+        break;
+      case 5:
+        onlyColorNo = getAfterChangeColor(color,3);
+        break;
+      case 6:
+        onlyColorNo = getAfterChangeColor(color,2);
+        break;
+      default:
+        onlyColorNo = getAfterChangeColor(color,1);
+        break;
+      }
+    return onlyColorNo;
+  }
+
+//更改已存在颜色No的某几个位,得到另外一个颜色NO
+  function getAfterChangeColor(color,n){
+    for (var i = 0;i < n;i++){
+      var index = getRandomIndexNo();
+      var newNo = getRandomNo();
+      //color = color.split("");//split方法找不到
+      //color.slice(index,1,newNo);
+      //console.log(color);
+      //color.join("");
+      color = replacePos(color,index,newNo);
+    }
+    return color;
+  }
+  function replacePos(strObj, pos, replacetext)
+  {
+    var str = strObj.substr(0, pos-1) + replacetext + strObj.substring(pos, strObj.length);
+    return str;
+  }
+//重新获取一个随机颜色
+  function getRandomColorNO(){
+    //第一种
+    return Math.floor(0x1000000+Math.random()*0x1000000).toString(16).slice(1);
+    //第二种
+    //(~~(Math.random()*(1<<24))).toString(16);
+  }
+  //获取一个更改颜色的颜色indexNO
+  function getRandomIndexNo(){
+    //可能为0,但总是小于1,所以要四舍五入处理,即可以取得正确范围闭区间[0,6]了
+    return parseInt(Math.random()*6);
+  }
+  //获取一个随机的16进制数字
+  function getRandomNo(){
+    return Math.floor(0x10+Math.random()*0x10).toString(16).slice(1);
   }
 }
 addRects();
